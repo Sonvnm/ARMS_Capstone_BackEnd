3f526076319cc68124be886fc75e55df6f20f6e3
anh020401 <zidarose112@gmail.com>
2024-10-09 22:39:10 +0700

    Add AccountController

---
+++ ARMS_API/Controllers/Admin/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ARMS_API.Controllers.Admin
+{
+    public class AccountController : ControllerBase
+    {
+        [HttpGet("get-accounts")]
+        public async Task<IActionResult> GetAccounts()
+        {
+            return Ok(Ok());
+        }
+        [HttpGet("get-account/{id}")]
+        public async Task<IActionResult> GetAccounts(Guid id)
+        {
+            return Ok(Ok());
+        }
+        
+        [HttpGet("get-accounts-student")]
+        public async Task<IActionResult> GetAccountsRequest()
+        {
+            return Ok(Ok());
+        }
+        [HttpPost("create-account")]
+        public async Task<IActionResult> CreateAccount()
+        {
+            return Ok(Ok());
+
+        }
+        [HttpPut("update-account/{id}")]
+        public async Task<IActionResult> UpdateAccount()
+        {
+            return Ok(Ok());
+        }
+    }
+}
