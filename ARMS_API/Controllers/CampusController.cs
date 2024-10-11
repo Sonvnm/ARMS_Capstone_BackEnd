6867dbac2143693895f0104cb8686554a0fa79dd
anh020401 <zidarose112@gmail.com>
2024-10-11 23:20:27 +0700

    Add CampusController

---
+++ ARMS_API/Controllers/CampusController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ARMS_API.Controllers
+{
+    public class CampusController : Controller
+    {
+        [Route("api/[controller]")]
+        [ApiController]
+        [HttpGet("count-campus")]
+        public async Task<IActionResult> CountCampus()
+        {
+            return Ok();
+        }
+        [HttpGet("get-campuses")]
+        public async Task<IActionResult> GetCampuses()
+        {
+            return Ok();
+        }
+        [HttpGet("get-campus")]
+        public async Task<IActionResult> GetCampusess()
+        {
+            return Ok();
+        }
+        [HttpGet("get-sliders")]
+        public async Task<IActionResult> GetSliders()
+        {
+            return Ok();
+        }
+        [HttpGet("get-history")]
+        public async Task<IActionResult> GetHistory()
+        {
+            return Ok();
+        }
+        [HttpGet("get-achievements")]
+        public async Task<IActionResult> GetAchievements()
+        {
+            return Ok();
+        }
+
+    }
+}
