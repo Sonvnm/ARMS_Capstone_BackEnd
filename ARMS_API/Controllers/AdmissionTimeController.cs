21c68fa0264d4775ff0b62bb3b49c9bfaabca1eb
anh020401 <zidarose112@gmail.com>
2024-10-11 23:17:39 +0700

    Add AdmissionTimeController

---
+++ ARMS_API/Controllers/AdmissionTimeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ARMS_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdmissionTimeController : Controller
+    {
+        public AdmissionTimeController()
+        {
+
+        }
+        [HttpGet("get-admission-time")]
+        public async Task<IActionResult> GetAdmissionTimes()
+        {
+            return Ok();
+        }
+    }
+}
