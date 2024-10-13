d09234d5a15ae345716e0a0353ae164868c654f0
anh020401 <zidarose112@gmail.com>
2024-10-13 23:28:22 +0700

    Add VNPayController

---
+++ ARMS_API/Controllers/VNPayController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ARMS_API.Controllers
+{
+    public class VNPayController : Controller
+    {
+        [HttpPost("pay-register-admission")]
+        public async Task<IActionResult> CreatePayment()
+        {
+            return Ok();
+        }
+        [HttpGet("vnpay_return")]
+        public IActionResult VNPayReturn()
+        {
+            return Ok();
+        }
+        [HttpPost("pay-admission")]
+        public async Task<IActionResult> CreatePaymentFeeAdmission()
+        {
+            return Ok();
+        }
+
+    }
+}
