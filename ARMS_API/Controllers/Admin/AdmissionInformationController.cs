e8b42f58868294b4ec22035cdabff24ba9c58408
Sonvnm <sonvu884@gmail.com>
2024-10-18 11:31:17 +0700

    Thêm AutoMapper với AdmissionInfor

--- ARMS_API/Controllers/Admin/AdmissionInformationController.cs
+++ ARMS_API/Controllers/Admin/AdmissionInformationController.cs
@@ -1,12 +1,20 @@
 ﻿using ARMS_API.ValidData;
 using AutoMapper;
+using Data.DTO;
+using Data.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Service.AdmissionInformationSer;
+using Service.AdmissionTimeSer;
 using Service.CampusSer;
 
 namespace ARMS_API.Controllers.Admin
 {
-    public class AdmissionInformationController : Controller
+    [Route("api/admin/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class AdmissionInformationController : ControllerBase
     {
         private IAdmissionInformationService _admissionInformationService;
         private ICampusService _campusService;
@@ -19,10 +27,31 @@ namespace ARMS_API.Controllers.Admin
             _campusService = campusService;
             _validAdmissionInformation = validAdmissionInformation;
         }
+
         [HttpPut("update-admission-information")]
-        public async Task<IActionResult> UpdateAdmissionInformation()
+        public async Task<IActionResult> UpdateAdmissionInformation(AdmissionInformation_Update_DTO AdmissionInformationDTO)
         {
-           return Ok();
+            try
+            {
+                var checkdata = await _campusService.GetCampus(AdmissionInformationDTO.CampusId);
+                if (checkdata == null) return NotFound();
+                //check data
+                _validAdmissionInformation.ValidDataAdmissionInfor(AdmissionInformationDTO);
+                //mapper
+                AdmissionInformation admissionInformation = _mapper.Map<AdmissionInformation>(AdmissionInformationDTO);
+                await _admissionInformationService.UpdateAdmissionInformation(admissionInformation);
+                return Ok(new ResponseViewModel()
+                {
+                    Status = true,
+                    Message = "Cập nhật thành công!"
+                });
+
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
         }
     }
 }
