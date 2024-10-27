93d227213d278870d3bc6b761de980466fae877d
anh020401 <zidarose112@gmail.com>
2024-10-28 00:33:07 +0700

    UpdateAdmisstionTime

--- Service/AdmissionTimeSer/AdmissionTimeService.cs
+++ Service/AdmissionTimeSer/AdmissionTimeService.cs
@@ -33,6 +33,46 @@ namespace Service.AdmissionTimeSer
             }
 
         }
+        public async Task UpdateAdmissionTime(AdmissionTime AdmissionTime)
+        {
+            try
+            {
+                var checkValid = await _admissionTimeRepository.GetAdmissionTime(AdmissionTime.AdmissionTimeId);
+                if (checkValid == null) throw new Exception("Không tìm thấy dữ liệu đợt tuyển sinh!");
+
+                var data = await _admissionTimeRepository.GetAdmissionTimes(checkValid.AdmissionInformationID);
+                var checkdata = data.Where(x => x.AdmissionTimeId != AdmissionTime.AdmissionTimeId)
+                            .OrderBy(x => x.EndRegister)
+                            .ToList();
+
+                var previousAdmission = checkdata.LastOrDefault(x => x.EndRegister < AdmissionTime.StartRegister);
+                if (previousAdmission != null)
+                {
+                    if (AdmissionTime.StartRegister <= previousAdmission.EndRegister)
+                    {
+                        throw new Exception("Thời gian bắt đầu không được trước hoặc bằng thời gian kết thúc của đợt tuyển sinh liền kề trước!");
+                    }
+                }
+                var nextAdmission = checkdata.FirstOrDefault(x => x.StartRegister > AdmissionTime.EndRegister);
+
+                if (nextAdmission != null && AdmissionTime.EndRegister >= nextAdmission.StartRegister)
+                {
+                    throw new Exception("Thời gian kết thúc không được sau hoặc bằng thời gian bắt đầu của đợt tuyển sinh liền kề sau!");
+                }
+                checkValid.AdmissionTimeName = AdmissionTime.AdmissionTimeName;
+                checkValid.EndAdmission = AdmissionTime.EndAdmission;
+                checkValid.StartAdmission = AdmissionTime.StartAdmission;
+                checkValid.StartRegister = AdmissionTime.StartRegister;
+                checkValid.EndRegister = AdmissionTime.EndRegister;
+                
+                await _admissionTimeRepository.UpdateAdmissionTime(checkValid);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
         public Task<AdmissionTime> GetAdmissionTime(int AIId)
             => _admissionTimeRepository.GetAdmissionTime(AIId);
         public async Task<List<AdmissionTime>> GetAdmissionTimes(string CampusId)
