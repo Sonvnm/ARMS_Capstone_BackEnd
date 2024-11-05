fece1ff3b5a230d8da25c639978e88cfcbd1d4f8
anh020401 <zidarose112@gmail.com>
2024-11-06 01:17:32 +0700

    Update AccountService

--- Service/AccountSer/AccountService.cs
+++ Service/AccountSer/AccountService.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Data.ArmsContext;
 using Data.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Repository;
 
 namespace Service.AccountSer
@@ -24,24 +25,87 @@ namespace Service.AccountSer
         public Task<Account> GetAccountByUserId(Guid userId)
        => _accountRepository.GetAccountByUserId(userId);
 
-        public Task<List<Account>> GetAccounts(string campusId)
+        public async Task<List<Account>> GetAccounts(string campusId)
         {
-            throw new NotImplementedException();
+            var allUsers = await _userManager.Users
+                    .Include(x => x.Major)
+                    .Where(user => user.CampusId == campusId && (user.TypeAccount == TypeAccount.Account || user.TypeAccount == TypeAccount.RequestAccountAccept))
+                    .ToListAsync();
+            return allUsers;
         }
 
-        public Task<List<Account>> GetAccountsRequest(string campusId)
+        public async Task<List<Account>> GetAccountsRequest(string campusId)
         {
-            throw new NotImplementedException();
+            var allUsers = await _userManager.Users
+                    .Include(x => x.Major)
+                    .Where(user => user.CampusId == campusId && (user.TypeAccount == TypeAccount.RequestAccountInProcess || user.TypeAccount == TypeAccount.RequestAccountRejected))
+                    .ToListAsync();
+            return allUsers;
         }
 
-        public Task<List<Account>> GetAccountStudent(string campusId)
+        public async Task<List<Account>> GetAccountStudent(string campusId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var studentRoleId = Guid.Parse("D2D63C5B-D09B-4828-8322-F18BA103FE86");
+
+                var allUsers = await _userManager.Users
+                    .Where(user => user.CampusId == campusId)
+                    .ToListAsync();
+
+                var studentAccounts = new List<Account>();
+                foreach (var user in allUsers)
+                {
+                    var roles = await _userManager.GetRolesAsync(user);
+                    if (roles.Any(roleName =>
+                    {
+                        var role = _roleManager.Roles.FirstOrDefault(r => r.Name == roleName);
+                        return role != null && role.Id == studentRoleId;
+                    }))
+                    {
+                        studentAccounts.Add(user);
+                    }
+                }
+
+                return studentAccounts;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Đã sảy ra lỗi!", ex);
+            }
         }
 
-        public Task<List<Account>> GetAO(string campusId)
+        public async Task<List<Account>> GetAO(string campusId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var RoleId = Guid.Parse("62378687-E16C-4D94-B767-DE9F0BFE9498");
+
+                var allUsers = await _userManager.Users
+                    .Where(user => user.CampusId == campusId)
+                    .ToListAsync();
+
+                var Accounts = new List<Account>();
+                foreach (var user in allUsers)
+                {
+                    var roles = await _userManager.GetRolesAsync(user);
+                    if (roles.Any(roleName =>
+                    {
+                        var role = _roleManager.Roles.FirstOrDefault(r => r.Name == roleName);
+                        return role != null && role.Id == RoleId;
+                    }))
+                    {
+                        Accounts.Add(user);
+                    }
+                }
+
+                return Accounts;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Đã sảy ra lỗi!", ex);
+            }
         }
+
     }
 }
