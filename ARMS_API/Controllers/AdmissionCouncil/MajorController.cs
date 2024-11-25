1853a9d6286ce5a30fe168667aca2790ca0f1a57
Anlq <lequangan2404@gmail.com>
2024-11-25 10:20:11 +0700

    Update Major Api for council, fix Program bên API

---
+++ ARMS_API/Controllers/AdmissionCouncil/MajorController.cs
@@ -0,0 +1,117 @@
+﻿using ARMS_API.ValidData;
+using AutoMapper;
+using Data.DTO;
+using Data.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Service.MajorSer;
+
+namespace ARMS_API.Controllers.AdmissionCouncil
+{
+    [Route("api/admission-council/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "AdmissionCouncil")]
+    public class MajorController : ControllerBase
+    {
+
+        private IMajorService _majorService;
+        private ValidMajor _validMajor;
+        private readonly IMapper _mapper;
+        private UserInput _userInput;
+        public MajorController(IMajorService majorService, IMapper mapper, ValidMajor validMajor, UserInput userInput)
+        {
+            _majorService = majorService;
+            _mapper = mapper;
+            _validMajor = validMajor;
+            _userInput = userInput;
+        }
+        [HttpPut("update-major")]
+        public async Task<IActionResult> UpdateMajor(Major_Admission_DTO MajorDTO)
+        {
+            try
+            {
+                MajorAdmission major = _mapper.Map<MajorAdmission>(MajorDTO);
+                await _majorService.UpdateMajorAdmission(major);
+                return Ok(new ResponseViewModel()
+                {
+                    Status = true,
+                    Message = "Cập nhật thành công!"
+                });
+
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+        [HttpGet("get-majors")]
+        public async Task<IActionResult> GetMajors(string? CampusId, bool? college, string? Search, int CurrentPage)
+        {
+            try
+            {
+                ResponeModel<MajorDTO> result = new ResponeModel<MajorDTO>();
+                result.CurrentPage = CurrentPage;
+                result.CampusId = CampusId;
+                result.Search = Search;
+
+                List<MajorAdmission> response = await _majorService.GetMajorsManage(CampusId);
+                if (college != null)
+                {
+                    response = response.Where(x => x.Major.isVocationalSchool == true).ToList();
+                }
+                // Search
+                if (!string.IsNullOrEmpty(Search))
+                {
+                    string searchTerm = _userInput.NormalizeText(Search);
+                    response = response
+                                .Where(major =>
+                                    major != null &&
+                                    (
+                                        _userInput.NormalizeText(major.Major.MajorName ?? "").Contains(searchTerm) ||
+                                        _userInput.NormalizeText(major.Major.MajorCode ?? "").Contains(searchTerm) ||
+                                        _userInput.NormalizeText(major.MajorID ?? "").Contains(searchTerm)
+                                    )
+                                )
+                                .ToList();
+                }
+
+                result.PageCount = (int)Math.Ceiling(response.Count() / (double)result.PageSize);
+                var majors = response
+                    .Skip(((int)result.CurrentPage - 1) * (int)result.PageSize)
+                    .Take((int)result.PageSize)
+                    .ToList();
+
+                List<MajorDTO> responeResult = _mapper.Map<List<MajorDTO>>(majors);
+                result.Item = responeResult;
+                result.TotalItems = response.Count;
+
+                return Ok(result);
+
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+        [HttpGet("get-major-details")]
+        public async Task<IActionResult> GetMajorDetail(string MajorId, int AdmissionInformationID)
+        {
+            try
+            {
+
+                MajorAdmission response = await _majorService.GetMajorDetail(MajorId, AdmissionInformationID);
+                MajorDTO responeResult = _mapper.Map<MajorDTO>(response);
+                return Ok(responeResult);
+
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+    }
+}
--- ARMS_API/Program.cs
+++ ARMS_API/Program.cs
@@ -1,16 +1,56 @@
-using System.Text;
+﻿using ARMS_API.Config;
+using ARMS_API.Helper;
+using ARMS_API.ValidData;
+using Data.ArmsContext;
+using Data.DTO;
+using Data.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using Repository;
+using Repository.MajorRepo;
+using Service;
+using Service.AccountSer;
+using Service.AdmissionInformationSer;
+using Service.AdmissionTimeSer;
+using Service.BlogSer;
+using Service.CampusSer;
+using Service.EmailSer;
+using Service.LocationSer;
+using Service.MajorSer;
+using Service.PayFeeAdmissionSer;
+using Service.PriorityService;
+using Service.RequestChangeMajorSer;
+using Service.RequestNotificationSer;
+using Service.StudentConsultationSer;
+using Service.StudentProfileServ;
+using Service.VnPaySer;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 ConfigurationManager configuration = builder.Configuration;
 // Add services to the container.
+builder.Services.AddDbContext<ArmsDbContext>(options => options.UseSqlServer(
+    configuration.GetConnectionString("ArmsConnectionString")
+));
 
-builder.Services.AddControllers();
+// create mapper
+var mapper = AutoMapperConfig.Initialize();
+builder.Services.AddSingleton(mapper);
+
+// For Identity
+builder.Services.AddIdentity<Account, IdentityRole<Guid>>()
+    .AddEntityFrameworkStores<ArmsDbContext>()
+    .AddDefaultTokenProviders();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 // Adding Authentication
 builder.Services.AddAuthentication(options =>
@@ -35,6 +75,25 @@ builder.Services.AddAuthentication(options =>
         ClockSkew = TimeSpan.FromDays(5)
     };
 });
+builder.Services.AddMemoryCache();
+builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowHttpOrigins", policy =>
+    {
+        // Add localhost:3000 to the allowed origins
+        policy.WithOrigins("https://localhost:5001", "http://localhost:3000") // Allow React's frontend
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+
 builder.Services.AddSwaggerGen(config =>
 {
     config.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger ARMS", Version = "v1" });
@@ -67,6 +126,46 @@ builder.Services.AddSwaggerGen(config =>
         }
     });
 });
+builder.Services.Configure<VNPaySettings>(configuration.GetSection("VNPay"));
+builder.Services.Configure<EmailSetting>(configuration.GetSection("EmailSettings"));
+
+//builder.Services.AddHttpsRedirection(options =>
+//{
+//    options.HttpsPort = 5001;
+//});
+
+//Config Repository 
+builder.Services.AddSingleton<TokenHealper>();
+builder.Services.AddScoped<UserInput>();
+builder.Services.AddScoped<ValidStudentConsultation>();
+builder.Services.AddScoped<ValidRegisterAdmission>();
+builder.Services.AddScoped<ValidBlog>();
+builder.Services.AddScoped<ValidMajor>();
+builder.Services.AddScoped<ValidAdmissionTime>();
+builder.Services.AddScoped<ValidAdmissionInformation>();
+
+builder.Services.AddScoped<ICampusService, CampusService>();
+builder.Services.AddScoped<IMajorService, MajorService>();
+builder.Services.AddScoped<IBlogService, BlogService>();
+builder.Services.AddScoped<IStudentConsultationService, StudentConsultationService>();
+builder.Services.AddScoped<IStudentProfileService, StudentProfileService>();
+builder.Services.AddScoped<IAdmissionTimeService, AdmissionTimeService>();
+builder.Services.AddScoped<IAdmissionInformationService, AdmissionInformationService>();
+builder.Services.AddScoped<IRequestService, RequestService>();
+builder.Services.AddScoped<IPriorityService, PriorityService>();
+builder.Services.AddScoped<IPayFeeAdmissionService, PayFeeAdmissionService>();
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<IRequestNotificationService, RequestNotificationService>();
+builder.Services.AddScoped<EmailQueue>();
+builder.Services.AddSingleton<FirebaseService>();
+//Services
+builder.Services.AddScoped<IEmailService, EmailService>();
+builder.Services.AddScoped<IEmailNotifyService, EmailNotifyService>();
+builder.Services.AddScoped<IVnPayService, VnPayService>();
+
+builder.Services.AddHttpClient(); // Thêm HttpClient vào DI container
+builder.Services.AddScoped<ILocationService, LocationService>();
+// app
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -75,9 +174,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
-
+app.UseCors("AllowHttpOrigins");
+// Authentication & Authorization
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
