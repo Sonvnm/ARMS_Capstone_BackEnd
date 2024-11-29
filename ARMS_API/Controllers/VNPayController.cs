9d4350d944edabc32b7e078344b7aa7c8e4390c1
anh020401 <zidarose112@gmail.com>
2024-11-30 01:50:55 +0700

    Add PayFeeAdmissionRepository

--- ARMS_API/Controllers/VNPayController.cs
+++ ARMS_API/Controllers/VNPayController.cs
@@ -1,9 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Service.AdmissionInformationSer;
+using Service.MajorSer;
 
 namespace ARMS_API.Controllers
 {
     public class VNPayController : Controller
     {
+        private readonly IVnPayService _vnPayService;
+        private readonly IMajorService _majorService;
+        private readonly IAdmissionInformationService _admissionInformationService;
+        private readonly IConfiguration _configuration;
+        public VNPayController(IVnPayService vnPayService, IAdmissionInformationService admissionInformationService, IMajorService majorService, IConfiguration configuration)
+        {
+            _vnPayService = vnPayService;
+            _admissionInformationService = admissionInformationService;
+            _majorService = majorService;
+            _configuration = configuration;
+        }
         [HttpPost("pay-register-admission")]
         public async Task<IActionResult> CreatePayment()
         {
