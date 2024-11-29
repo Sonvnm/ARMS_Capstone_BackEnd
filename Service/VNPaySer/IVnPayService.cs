29287463bbbd91167b7777039a5d1a1b564528d3
anh020401 <zidarose112@gmail.com>
2024-11-30 01:55:02 +0700

    Add VnpayService

---
+++ Service/VNPaySer/IVnPayService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace Service.VNPaySer
+{
+    public interface IVnPayService
+    {
+        string CreatePaymentUrl(HttpContext context, Guid codePayment, decimal fee, DateTime dateCreate);
+        string CreatePaymentUrlAdmission(HttpContext context, Guid codePayment, decimal fee, DateTime dateCreate);
+        PayFeeAdmission PaymentExecute(IQueryCollection collections);
+    }
+}
