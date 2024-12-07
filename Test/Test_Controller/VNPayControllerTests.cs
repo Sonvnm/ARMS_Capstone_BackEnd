dc24f216ec23935e5d6b93fa0492af10e9d6c8df
anh020401 <zidarose112@gmail.com>
2024-12-07 08:39:01 +0700

    Update Test 2

--- Test/Test_Controller/VNPayControllerTests.cs
+++ Test/Test_Controller/VNPayControllerTests.cs
@@ -11,6 +11,7 @@ using Moq;
 using Service.AdmissionInformationSer;
 using Service.MajorSer;
 using Service.VnPaySer;
+using Service.VNPaySer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
